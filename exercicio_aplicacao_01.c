cde1c7be66c805d60b872888b79ef5817ac1e105
Wagner Alves Moreira <wagneramoreira06@gmail.com>
2026-08-26 19:55:39 -0300

    Remove existing implementation of circumference calculation

--- exercicio_aplicacao_01.c
+++ exercicio_aplicacao_01.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#define PI 3.14159
 
-int main() {
-    
-    
-    float raio, perimetro;
-    
-    printf("Digite a medida do raio (em cm): ");
-    scanf("%f", &raio);
-    perimetro = 2*PI*raio;
-    printf("O perimetro total da circunferência é (em cm): %.2f", perimetro);
-   
-    
-    return 0;
-    
-}    
